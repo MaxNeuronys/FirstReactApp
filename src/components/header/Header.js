4b5ea1def517e5d403c9ef6aa22aeabad225f5f6
MaxNeuronys <max@neuronys.ai>
2020-01-06 18:53:01 +0100

    header in progress

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,18 +2,25 @@ import React, { Component } from 'react'
 
 export default class Header extends Component {
 
-    render(){
-        return <header className="navbar navbar-extend-lg navbar-light bg-light">
-            <a className="navbar-brand">Street-Feed </a>
+    render() {
+        return (
+        <header className="navbar navbar-extend-lg navbar-light bg-light">
+            <a className="navbar-brand" href="/">Street-Feed </a>
             <button className="navbar-toggler">
-            <span className="navbar-toggler-icon"></span>
+                <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse">
-            <ul className=" navbar-nav ml-auto">
-                <li className="nav-item active"></li>
-            </ul>
+                <ul className=" navbar-nav ml-auto">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="/">Accueil</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/">Favoris</a>
+                    </li>
+                </ul>
             </div>
         </header>
+        )
     }
 
 }
