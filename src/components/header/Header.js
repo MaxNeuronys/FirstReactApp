c0af4ea39037feed952ecb6ff55fdb8e8089170c
MaxNeuronys <max@neuronys.ai>
2020-01-06 18:37:36 +0100

    header first worked

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,7 +3,17 @@ import React, { Component } from 'react'
 export default class Header extends Component {
 
     render(){
-        return <header>Header</header>
+        return <header className="navbar navbar-extend-lg navbar-light bg-light">
+            <a className="navbar-brand">Street-Feed </a>
+            <button className="navbar-toggler">
+            <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse">
+            <ul className=" navbar-nav ml-auto">
+                <li className="nav-item active"></li>
+            </ul>
+            </div>
+        </header>
     }
 
 }
